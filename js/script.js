81d1ef3ea9eca6d7c91b0f032c0e679b9a72dd57
Jime Sanchez <jimeesanchez.-@hotmail.com>
2024-11-02 11:08:13 -0300

    nuevos cambios entrega2

--- js/script.js
+++ js/script.js
@@ -1,34 +1,8 @@
-class Usuario {
-    constructor(nombre, contrasena) {
-        this.nombre = nombre;
-        this.contrasena = contrasena;
-    }
-}
-
+// aca declaro las variables
 const usuarios = [
     new Usuario("usuario1", "contrasena1"),
     new Usuario("usuario2", "contrasena2"),
 ];
-
-function verificarCredenciales(nombre, contrasena) {
-    for (let usuario of usuarios) {
-        if (usuario.nombre === nombre && usuario.contrasena === contrasena) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function iniciarSesion() {
-    let nombre = prompt("Por favor, ingresa tu nombre de usuario:");
-    let contrasena = prompt("Por favor, ingresa tu contraseña:");
-
-    if (verificarCredenciales(nombre, contrasena)) {
-        alert("¡Inicio de sesión exitoso!");
-    } else {
-        alert("Nombre de usuario o contraseña incorrectos.");
-    }
-}
 
 let usuario = {
     nombre: '',
@@ -37,19 +11,41 @@ let usuario = {
 
 let resultados = [];
 
-function capturarDatos() {
+class Usuario {
+    constructor(nombre, contrasena) {
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+    }
+}
+
+// converti la funcion flecha como se me aconsejo
+const verificarCredenciales = (nombre, contrasena) => {
+    return usuarios.some(usuario => usuario.nombre === nombre && usuario.contrasena === contrasena);
+};
+
+const iniciarSesion = () => {
+    const nombre = prompt("Por favor, ingresa nombre de usuario:");
+    const contrasena = prompt("Por favor, ingresa contraseña:");
+
+    if (verificarCredenciales(nombre, contrasena)) {
+        alert("¡Inicio de sesión exitoso!");
+    } else {
+        alert("Nombre de usuario o contraseña incorrectos.");
+    }
+};
+
+const capturarDatos = () => {
     usuario.nombre = prompt("Ingresa tu nombre:");
     usuario.edad = parseInt(prompt("Ingresa tu edad:"), 10);
-}
+};
 
-function procesarDatos() {
-    let nuevaEdad = usuario.edad + 10;
+const procesarDatos = () => {
+    const nuevaEdad = usuario.edad + 10;
     resultados.push(nuevaEdad);
-}
+};
 
-function mostrarResultados() {
+const mostrarResultados = () => {
     alert(`Hola ${usuario.nombre}, tu edad en 10 años será: ${resultados[0]}`);
-}
-
+};
 
 iniciarSesion();
